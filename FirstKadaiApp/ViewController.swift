f4b90a756ee0ddfbe22dbf090cfc8c55c2a63b25
増田華子 <masudahanako@masudakazokunoimac.local>
2017-12-25 15:29:00 +0900

    新しいラベルを追加した。 アプリを開くとラベル１が赤くなるようにした。

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label1.textColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
